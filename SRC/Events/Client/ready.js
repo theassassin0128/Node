89f28a08581185dec87a8dc0eff55672ae2851ba
THE-ASSASSIN0128 <rafisarkar46@gmail.com>
2022-10-24 13:10:50 +0600

    V-0.0.3 #Event-Hanlder

---
+++ SRC/Events/Client/ready.js
@@ -0,0 +1,40 @@
+const ascii = require("ascii-table");
+const { bot } = require(`${process.cwd()}/SRC/config.json`);
+const { DataBase } = process.env;
+const mongoose = require("mongoose");
+
+module.exports = {
+  name: "ready",
+  once: true,
+  execute: async (client) => {
+    client.user.setPresence({
+      activities: [
+        {
+          type: "LISTENING",
+          name: "?help",
+        },
+      ],
+      status: "idle",
+    });
+
+    const Table = new ascii("BOT");
+
+    Table.addRow("tag", `${client.user.tag}`);
+    Table.addRow("id", `${client.user.id}`);
+    Table.addRow("prefix", `${bot.prefix}`);
+    Table.addRow("guild(s)", `${client.guilds.cache.size}`);
+
+    console.log(Table.toString(), `\nReady! Logged in as ${client.user.tag}`);
+
+    if (!DataBase) return;
+    try {
+      mongoose.connect(DataBase, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("[🟢] DataBase Connected.");
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
--- SRC/index.js
+++ SRC/index.js
@@ -1,37 +1,35 @@
 const {
-    Client,
-    Collection,
-    Partials
+  Client,
+  Collection,
+  Partials,
+  GatewayIntentBits,
+  Events,
 } = require("discord.js");
 require("dotenv").config();
-const { Token, DataBase } = process.env;
-  
-  
-//All Functions
-/*const { loadEvents } = require(`./Handlers/Events.js`);
-const { loadCommands } = require(`./Handlers/Commands.js`);*/
-  
-  
+const { Token } = process.env;
+
 //Making a client property to use globally.
 const client = new Client({
-    intents: 3276799,
-    partials: [
-        Partials.Channel,
-        Partials.User
-    ],
-    allowedMentions: {
-        repliedUser: false
-    }
+  intents: 3276799,
+  partials: [
+    Partials.User,
+    Partials.Message,
+    Partials.GuildMember,
+    Partials.ThreadMember,
+  ],
+  allowedMentions: {
+    repliedUser: false,
+  },
 });
 
-//Collections to store message/slash commands.
+const { loadEvents } = require("./Functions/Handlers/events.js");
+
+//Collections to store data
+client.events = new Collection();
 client.commands = new Collection();
 client.aliases = new Collection();
-  
-  
+
+loadEvents(client);
+
 //Connect to your bot by using a token (provided by discord)
-try {
-    client.login(Token);
-} catch (error) {
-    console.error(error);
-};
+client.login(Token).catch((err) => console.log(err));
