e06687967477d29a30f6ec19ca0ee992fc13501d
THE-ASSASSIN0128 <rafisarkar46@gmail.com>
2022-12-06 16:48:06 +0600

    clear.js -1

--- src/commands/moderation/clear.js
+++ src/commands/moderation/clear.js
@@ -1,42 +1,59 @@
 const {
-    SlashCommandBuilder,
-    Client,
-    PermissionFlagsBits,
+	SlashCommandBuilder,
+	Client,
+	PermissionFlagsBits,
+	ChatInputCommandInteraction,
 } = require("discord.js");
 
 module.exports = {
-    developer: true,
-    data: new SlashCommandBuilder()
-        .setName("clear")
-        .setDescription("Delete a certain amount of messages.")
-        .addIntegerOption((option) =>
-            option
-                .setName("amount")
-                .setDescription("Number of messages to delete.")
-                .setRequired(true)
-        )
-        .setDMPermission(false)
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
-    /**
-     *
-     * @param {ChatInputCommandInteraction} interaction
-     * @param {Client} client
-     */
-    execute: async (interaction, client) => {
-        const { member, guild, user } = interaction;
+	developer: true,
+	data: new SlashCommandBuilder()
+		.setName("clear")
+		.setDescription("Delete a certain amount of messages.")
+		.addIntegerOption((option) =>
+			option
+				.setName("amount")
+				.setDescription("Number of messages to delete.")
+				.setRequired(true)
+		)
+		.addUserOption((option) =>
+			option.setName("member").setDescription("Pick a guild member.")
+		)
+		.setDMPermission(false)
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+	/**
+	 *
+	 * @param {ChatInputCommandInteraction} interaction
+	 * @param {Client} client
+	 */
+	execute: async (interaction, client) => {
+		const { user, options } = interaction;
 
-        const amount = interaction.options.getInteger("amount");
+		const amount = options.getInteger("amount");
+		const mUser = options.getUser("member");
 
-        if (100 < amount < 0) {
-            return interaction.reply(
-                "Please, enter a valid amount between **1-100**"
-            );
-        }
+		if (100 < amount < 0) {
+			return interaction.reply(
+				"Please, enter a valid amount between **1-100**"
+			);
+		}
 
-        const dMsg = await interaction.channel.bulkDelete(amount, true);
-        interaction.reply({
-            content: `Successfully deleted ${dMsg.size} messages.`,
-            ephemeral: true,
-        });
-    },
+		if (mUser) {
+			const msg = await await interaction.channel.messages.fetch();
+			let i = [];
+			const uMsg = await msg.filter((m) => m.author.id === mUser.id);
+			i.push(uMsg);
+			const dMsg = await interaction.channel.bulkDelete(uMsg, true);
+
+			interaction.reply({
+				content: `Deleted ${dMsg.size} of ${mUser}.`,
+			});
+		} else {
+			const dMsg = await interaction.channel.bulkDelete(amount, true);
+			interaction.reply({
+				content: `Successfully deleted ${dMsg.size} messages.`,
+				ephemeral: true,
+			});
+		}
+	},
 };
