57a322549ebb2d60f50f7c2a5be8eacb1e6a24ba
THE-ASSASSIN0128 <rafisarkar46@gmail.com>
2022-12-18 22:48:41 +0600

    littlechange

--- src/commands/information/user.js
+++ src/commands/information/user.js
@@ -3,6 +3,7 @@ const {
 	SlashCommandBuilder,
 	Client,
 	ChatInputCommandInteraction,
+	ReactionUserManager,
 } = require("discord.js");
 const { colour } = require("../../config.json");
 const moment = require("moment");
@@ -10,7 +11,7 @@ const moment = require("moment");
 module.exports = {
 	developer: true,
 	data: new SlashCommandBuilder()
-		.setName("user-info")
+		.setName("user")
 		.setDescription("Replies an embed containing user information")
 		.addUserOption((options) =>
 			options.setName("user").setDescription("The user")
@@ -27,22 +28,33 @@ module.exports = {
 			const User = interaction.guild.members.cache.get(user.id);
 
 			let Roles = User.roles.cache;
+			let colour = User.roles.color.hexColor;
 
-			let info1 = new EmbedBuilder()
-				.setTitle(`${user.tag}`)
-				.setImage()
-				.setDescription(
-					`**Nick Name : ${User.displayName} | ${user}\nUser Name: ${User.user.username} \nID : __${user.id}__**`
-				)
+			const info = new EmbedBuilder()
+				.setAuthor({
+					name: `${user.username}'s Information`,
+					iconURL: `${user.displayAvatarURL({ size: 2048 })}`,
+				})
+				.setThumbnail(User.displayAvatarURL({ size: 4096 }))
 				.addFields(
 					{
-						name: "Joined Server",
-						value: `${moment(User.joinedAt).format(
-							"dddd, MMMM Do YYYY, h:mm:ss A"
-						)}\n** - ${moment(
-							User.joinedAt,
-							"YYYYMMDD"
-						).fromNow()}**`,
+						name: "**__General Information__**",
+						value: [
+							`**:identification_card: Name** : ${user.tag}`,
+							`**:id: ID** : ${user.id}`,
+							`**:placard: Nickname** : ${User.displayName} || None`,
+							`**:robot: Bot** : ${
+								user.bot ? ":white_check_mark: Yes" : ":x: No"
+							} `,
+							`**Joined Server** : \n${moment(
+								User.joinedAt
+							).format(
+								"dddd, MMMM Do YYYY, h:mm:ss A"
+							)}\n** - ${moment(
+								User.joinedAt,
+								"YYMMDD"
+							).fromNow()}**`,
+						].join("\n"),
 					},
 					{
 						name: "Joined Discord",
@@ -58,15 +70,13 @@ module.exports = {
 						value: "More info will be added in the next update.",
 					}
 				)
-				.setColor(colour.main);
+				.setColor(colour);
 
 			interaction.reply({
-				embeds: [info1],
+				embeds: [info],
 			});
 		} catch (err) {
-			interaction.reply({
-				content: `${err}`,
-			});
+			console.error(err);
 		}
 	},
 };
