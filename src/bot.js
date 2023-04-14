94323ad9fe3ea96b84fa5a7a234363986bde49a2
THE-ASSASSIN0128 <rafisarkar46@gmail.com>
2023-04-15 00:11:28 +0600

    updated bot.js #Client

--- src/bot.js
+++ src/bot.js
@@ -1,13 +1,51 @@
-const { Client, Collection, Partials } = require("discord.js");
+const { Client, Collection, Partials, IntentsBitField } = require("discord.js");
 
+// All the required intents.
+const {
+  Guilds,
+  GuildMembers,
+  GuildEmojisAndStickers,
+  GuildMessageReactions,
+  GuildMessages,
+  MessageContent,
+  DirectMessages,
+  DirectMessageReactions,
+  GuildIntegrations,
+  GuildModeration,
+  GuildWebhooks,
+  GuildVoiceStates,
+  GuildPresences,
+} = IntentsBitField.Flags;
+
+// All the required Partials
+const {
+  User,
+  Message,
+  GuildMember,
+  ThreadMember,
+  Channel,
+  Reaction,
+  GuildScheduledEvent,
+} = Partials;
+
+// Creating a discord client to use globally
 const client = new Client({
-  intents: 65471,
-  partials: [
-    Partials.User,
-    Partials.Message,
-    Partials.GuildMember,
-    Partials.ThreadMember,
+  intents: [
+    Guilds,
+    GuildMembers,
+    GuildEmojisAndStickers,
+    GuildMessageReactions,
+    GuildMessages,
+    MessageContent,
+    DirectMessages,
+    DirectMessageReactions,
+    GuildIntegrations,
+    GuildModeration,
+    GuildWebhooks,
+    GuildVoiceStates,
+    GuildPresences,
   ],
+  partials: [User, Message, GuildMember, ThreadMember, Channel, Reaction],
   allowedMentions: {
     repliedUser: false,
   },
